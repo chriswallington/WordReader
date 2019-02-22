ec52468748d94fadf7b07c7032a11aa33c60624b
Chris Wallington <wallingtonc@vet.local>
2019-02-22 09:21:50 +0000

    Add project files.

---
+++ WordReader/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = "";
+
+// had a look at if statements, but effectively runs once. While will execute until condition is met
+
+            while (words != "x")
+            {
+                Console.WriteLine("Enter some words");
+                words = Console.ReadLine();
+                Console.WriteLine("Words entered: " + words);
+                Console.WriteLine("Number of Letters: " + words.Length);
+
+                string wordsbackwards = string.Join(" ", words.Split(' ').Select(x => new String(x.Reverse().ToArray())));
+                Console.WriteLine("Words in reverse order: {0}", wordsbackwards);
+
+                StringBuilder sb = new StringBuilder();
+                string[] split = words.Split(' ');
+                for (int i = split.Length - 1; i > -1; i--)
+                {
+                    sb.Append(split[i]);
+                    sb.Append(" ");
+                }
+                Console.WriteLine("String Builder Reverse: {0}",sb);
+            }
+
+
+        }
+    }
+
+
+    //class Words
+    //{
+    //    public string word { get; set; }
+    //}
+}
